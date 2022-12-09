7f2b30e455521f17abeee41d2ccdd7cd16d61b2d
dubtcs <D3080>
2022-12-08 20:36:46 -0500

    Premake bat files

---
+++ GENERATE_AND_DELETE.bat
@@ -0,0 +1,6 @@
+del AxtEngine\AxtEngine.vcxproj
+del AxtEngine\AxtEngine.vcxproj.filters
+del Sandbox\Sandbox.vcxproj
+del AxtEngine.sln
+call vendor\bin\premake\premake5.exe vs2022
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,99 @@
+
+workspace "AxtEngine"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+local output = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "AxtEngine"
+    location "AxtEngine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/"..output.."/%{prj.name}")
+    objdir ("bin-int/"..output.."/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "10.0.19041.0"
+
+        defines {
+            "AXT_BUILD_DLL";
+            "AXT_PLATFORM_WINDOWS";
+        }
+
+        postbuildcommands{
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/"..output.."/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines {"AXT_DEBUG"}
+        symbols "On"
+
+    filter "configurations:Release"
+        defines {"AXT_RELEASE"}
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines {"AXT_DIST"}
+        symbols "On"
+
+-- SANDBOX
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/"..output.."/%{prj.name}")
+    objdir ("bin-int/"..output.."/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        "AxtEngine/vendor/spdlog/include",
+        "AxtEngine/src"
+    }
+
+    links {
+        "AxtEngine"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "10.0.19041.0"
+
+        defines {
+            "AXT_PLATFORM_WINDOWS";
+        }
+
+    filter "configurations:Debug"
+        defines "AXT_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "AXT_RELEASE"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines "AXT_DIST"
+        symbols "On"
